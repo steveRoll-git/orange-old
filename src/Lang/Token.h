87f86c0d09ff3d6b7418792c1bb08170a64e8275
Ron Tseytlin <ronts390@gmail.com>
2021-01-11 15:42:41 +0200

    added copy constructor (same as assignment)

--- src/Lang/Token.h
+++ src/Lang/Token.h
@@ -33,6 +33,19 @@ namespace Orange::Lang
         Token(TokenType _type, std::string _string) : type(_type), string(_string) {};
         Token(double _number) : type(TokenType::Number), number(_number) {};
 
+        Token(const Token& other)
+        {
+            type = other.type;
+            if (other.type == TokenType::Identifier || other.type == TokenType::String)
+            {
+                string = other.string;
+            }
+            else if (other.type == TokenType::Number)
+            {
+                number = other.number;
+            }
+        }
+
         void operator =(const Token& other)
         {
             type = other.type;
