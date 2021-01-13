9cf0f4f9c061e3bdf58a981bd06e013550189d58
Ron Tseytlin <ronts390@gmail.com>
2021-01-13 23:56:29 +0200

    added copy methods for Value

--- src/Value.h
+++ src/Value.h
@@ -26,17 +26,76 @@ namespace Orange
 		union
 		{
 			NumberType number;
-			char* string;
-			//char* symbol;
+			std::string string;
 			ConsCell* cons;
 		};
 
+		bool valueSet = false;
+
 		Value() : type(ValueType::Nil) {};
-		Value(ValueType _type) : type(_type) {};
-		Value(ValueType _type, NumberType _number) : type(_type), number(_number) {};
-		Value(ValueType _type, char* _string) : type(_type), string(_string) {};
-		//Value(ValueType _type, char* _symbol) : type(_type), symbol(_symbol) {};
+		Value(ValueType _type) : type(_type)
+		{
+			if (type == ValueType::Symbol || type == ValueType::String)
+			{
+				new(&string) std::string;
+			}
+		}
+		Value(ValueType _type, NumberType _number) : type(_type)
+		{
+			setValue(_number);
+		}
+		Value(ValueType _type, std::string& _string) : type(_type)
+		{
+			setValue(_string);
+		}
 		Value(ValueType _type, ConsCell* _cons) : type(_type), cons(_cons) {};
+
+		void copyOther(Value& other)
+		{
+			type = other.type;
+			if ((other.type == ValueType::Symbol || other.type == ValueType::String) && other.valueSet)
+			{
+				setValue(other.string);
+			}
+			else if (other.type == ValueType::Number)
+			{
+				number = other.number;
+			}
+			else if (other.type == ValueType::List)
+			{
+				cons = other.cons;
+			}
+
+		}
+
+		Value(Value& other)
+		{
+			copyOther(other);
+		}
+		void operator =(Value& other)
+		{
+			copyOther(other);
+		}
+
+		void setValue(std::string& _string)
+		{
+			new(&string) std::string;
+			string = _string;
+			valueSet = true;
+		}
+		void setValue(double _number)
+		{
+			number = _number;
+			valueSet = true;
+		}
+
+		~Value()
+		{
+			if ((type == ValueType::Symbol || type == ValueType::String) && valueSet)
+			{
+				string.~string();
+			}
+		}
 
 		const char* getTypeName();
 		std::string toString(bool hideListParens = false);
